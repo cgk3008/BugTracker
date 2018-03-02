64b2c9a217c31633f16722eeeade448e7586c034
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-03-02 15:28:20 -0500

    added template, still need user and project helpers completed, also the controlllers for my tixcomments, notifications etc are commented out because they had bad references

--- BugTracker/Controllers/TicketNotificationsController.cs
+++ BugTracker/Controllers/TicketNotificationsController.cs
@@ -8,129 +8,129 @@ using System.Web;
 using System.Web.Mvc;
 using BugTracker.Models;
 
-namespace BugTracker.Controllers
-{
-    public class TicketNotificationsController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+//namespace BugTracker.Controllers
+//{
+//    public class TicketNotificationsController : Controller
+//    {
+//        private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: TicketNotifications
-        public ActionResult Index()
-        {
-            var ticketNotifications = db.TicketNotifications.Include(t => t.Ticket).Include(t => t.User);
-            return View(ticketNotifications.ToList());
-        }
+//        // GET: TicketNotifications
+//        public ActionResult Index()
+//        {
+//            var ticketNotifications = db.TicketNotifications.Include(t => t.Ticket).Include(t => t.User);
+//            return View(ticketNotifications.ToList());
+//        }
 
-        // GET: TicketNotifications/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
-            if (ticketNotification == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ticketNotification);
-        }
+//        // GET: TicketNotifications/Details/5
+//        public ActionResult Details(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
+//            if (ticketNotification == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(ticketNotification);
+//        }
 
-        // GET: TicketNotifications/Create
-        public ActionResult Create()
-        {
-            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Body");
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName");
-            return View();
-        }
+//        // GET: TicketNotifications/Create
+//        public ActionResult Create()
+//        {
+//            ViewBag.TicketId = new SelectList(db.Ticket, "Id", "Body");
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName");
+//            return View();
+//        }
 
-        // POST: TicketNotifications/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,TicketId,UserId")] TicketNotification ticketNotification)
-        {
-            if (ModelState.IsValid)
-            {
-                db.TicketNotifications.Add(ticketNotification);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+//        // POST: TicketNotifications/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include = "Id,TicketId,UserId")] TicketNotification ticketNotification)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.TicketNotifications.Add(ticketNotification);
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
 
-            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Body", ticketNotification.TicketId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
-            return View(ticketNotification);
-        }
+//            ViewBag.TicketId = new SelectList(db.Ticket, "Id", "Body", ticketNotification.TicketId);
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
+//            return View(ticketNotification);
+//        }
 
-        // GET: TicketNotifications/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
-            if (ticketNotification == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Body", ticketNotification.TicketId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
-            return View(ticketNotification);
-        }
+//        // GET: TicketNotifications/Edit/5
+//        public ActionResult Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
+//            if (ticketNotification == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            ViewBag.TicketId = new SelectList(db.Ticket, "Id", "Body", ticketNotification.TicketId);
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
+//            return View(ticketNotification);
+//        }
 
-        // POST: TicketNotifications/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,TicketId,UserId")] TicketNotification ticketNotification)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(ticketNotification).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Body", ticketNotification.TicketId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
-            return View(ticketNotification);
-        }
+//        // POST: TicketNotifications/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include = "Id,TicketId,UserId")] TicketNotification ticketNotification)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Entry(ticketNotification).State = EntityState.Modified;
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            ViewBag.TicketId = new SelectList(db.Ticket, "Id", "Body", ticketNotification.TicketId);
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketNotification.UserId);
+//            return View(ticketNotification);
+//        }
 
-        // GET: TicketNotifications/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
-            if (ticketNotification == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ticketNotification);
-        }
+//        // GET: TicketNotifications/Delete/5
+//        public ActionResult Delete(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
+//            if (ticketNotification == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(ticketNotification);
+//        }
 
-        // POST: TicketNotifications/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
-            db.TicketNotifications.Remove(ticketNotification);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+//        // POST: TicketNotifications/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(int id)
+//        {
+//            TicketNotification ticketNotification = db.TicketNotifications.Find(id);
+//            db.TicketNotifications.Remove(ticketNotification);
+//            db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
-}
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
--- BugTracker/Models/Helper/ProjectHelper.cs
+++ BugTracker/Models/Helper/ProjectHelper.cs
@@ -8,6 +8,17 @@ namespace BugTracker.Models.Helper
     public class ProjectHelper
     {
 
+        //        Your project helper class should include but is not limited to...
+
+        //IsUserOnProject
+        //ListUserProjects-done?
+        //AddUserToProjectdone?
+        //RemoveUserFromProject-done?
+        //UsersOnProject-done?
+        //UserNotOnProject(edited)-done?
+
+
+
         private ApplicationDbContext db = new ApplicationDbContext();
 
         public Exception AddUserToProject(string userId, int projectId)
@@ -26,7 +37,6 @@ namespace BugTracker.Models.Helper
             }
         }
 
-
         public Exception RemoveUserFromProject(string userId, int projectId)
         {
             try
@@ -43,12 +53,12 @@ namespace BugTracker.Models.Helper
             }
         }
 
-
         public ICollection<Project> ListProjectsForUser(string userId)
         {
 
             return db.Users.Find(userId).Project.ToList();
 
+            //all this code simplified above
             //var usr = db.Users.Find(userId);
             //List<Project> ProjUsers = new List<Project>();
 
@@ -68,11 +78,55 @@ namespace BugTracker.Models.Helper
             //{
             //    Console.WriteLine(ex);
             //    return null;
-            //}
+            //}                
+        }
+        //add function to not allow same user on same project twice!!!
 
 
+        //IsUserOnProject
+
+        //public bool IsUserOnProject(string userId, int projectId)
+        //{
+        //    try
+        //    {
+        //        var prj = db.Project.Find(projectId);
+        //        var usr = db.Users.Find(userId);
+        //        prj.User.Find(usr);
+
+        //        var result = db.Users.Find(userId);
+        //        if (
+        //        result != null)
+        //            return true;
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //        return false;
+        //    }
+        //}
+
+        public ICollection<User> ListUsersOnProject(int projectId)
+        {
+            return db.Project.Find(projectId).User.ToList();
         }
 
+        //UserNotOnProject(edited)
+
+        //    public ICollection<User> ListUsersNotOnProject(int projectId)
+        //    {
+        //        List<User> projectUsers = new List<User>();
+        //        List<User> users = db.Users.ToList();
+
+        //        foreach (var u in users)
+        //        {
+        //            if (!IsUserOnProject(u.Id, projectId))
+        //            {
+        //                projectUsers.Add(u);
+        //            }
+        //        }
+        //        return projectUsers;
+        //    }
 
 
     }
